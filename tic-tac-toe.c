812c29a1336ba2ee148de47009c84b40c074e5fe
Katherine Co <katherineco@cpp.edu>
2022-10-07 09:53:55 -0700

    Initial file with comments

---
+++ tic-tac-toe.c
@@ -0,0 +1,28 @@
+//Katherine Co
+//CS2600
+//Quiz 2: Tic-Tac-Toe
+
+
+// Tic Tac Toe game between 2 players or player vs computer
+int main()
+{
+    // Prompt user for game they wish to play (person vs. person or player vs. computer)
+
+    // Read information from console
+
+    // Display a blank board game
+
+    // Ask the player to make their first move and display their move on the board
+
+        // Detect invalid inputs from the user
+
+    // Continue to play back and forth until there is a win
+
+    // If there is a win, immediately display who won
+
+    // If there is a tie, immediately stop the program and say it was a tie
+
+    // Prompt the user if they would like to play again
+  
+    return 0;
+}
